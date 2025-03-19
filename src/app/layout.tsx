5fa07bde529589329fcf2d469d48971e752540a4
jorge soares <jorgesoares@jorges-MacBook-Air.local>
2025-03-19 00:33:46 -0300

    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import ClientParticle from "@/components/ClientParticle";
 
 export const metadata: Metadata = {
-  title: "My Neon Next App",
+  title: "Jorge Soares portfolio",
   description: "A disruptive neon-themed Next.js project",
 };
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt">
       {" "}
-      {/* Pode ser fixo ou ajustado dinamicamente se desejar */}
+
       <body className="min-h-screen flex flex-col bg-dark-bg text-neon-blue">
         <ClientParticle />
         <Navbar />
